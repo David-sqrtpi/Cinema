6a130c626bbba6398cddb05e5dd7328c1b47cb11
David-sqrtpi <david.stiven911@gmail.com>
2024-05-08 21:44:17 -0500

    SQLite migrations for the Functions service.

---
+++ Cinema.Services.Ticket/SqliteMigrations/20240506035742_Initial-Sqlite.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cinema.Services.Ticket.SqliteMigrations
+{
+    /// <inheritdoc />
+    public partial class InitialSqlite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    TicketId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FunctionId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    AdditionalPrice = table.Column<decimal>(type: "TEXT", nullable: true),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    Seats = table.Column<int>(type: "INTEGER", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.TicketId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
